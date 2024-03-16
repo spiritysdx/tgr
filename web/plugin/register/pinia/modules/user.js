13082c8c670de5ebea8de30b64010885b2ece171
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-03-16 17:22:34 +0800

    Update user.js

--- web/plugin/register/pinia/modules/user.js
+++ web/plugin/register/pinia/modules/user.js
@@ -1,34 +1,42 @@
 import { useUserStore } from '@/pinia/modules/user'
 import { ElLoading } from 'element-plus'
 import { userRegister } from '@/plugin/register/api/api'
+import { defineStore } from 'pinia'
 
 // 获取原始的 useUserStore 模块
 const originalUserStore = useUserStore()
 
 // 扩展原始的 useUserStore 模块
-export const extendedUseUserStore = originalUserStore.extend({
-  Register: async (loginInfo) => {
-    const loadingInstance = ElLoading.service({
+export const extendedUseUserStore = defineStore({
+  id: 'user',
+  state: () => ({
+    userInfo: null,
+    token: null
+  }),
+  actions: {
+    async Register(loginInfo) {
+      const loadingInstance = ElLoading.service({
         fullscreen: true,
         text: "注册中，请稍候...",
-    });
-    try {
+      });
+      try {
         const res = await userRegister(loginInfo);
         if (res.code === 0) {
-            originalUserStore.userInfo = res.data.user;
-            originalUserStore.token = res.data.token;
-            const routerStore = originalUserStore.useRouterStore();
-            await routerStore.SetAsyncRouter();
-            const asyncRouters = routerStore.asyncRouters;
-            asyncRouters.forEach((asyncRouter) => {
-                router.addRoute(asyncRouter);
-            });
-            router.push({name: originalUserStore.userInfo.authority.defaultRouter});
-            return true;
+          this.userInfo = res.data.user;
+          this.token = res.data.token;
+          const routerStore = this.useRouterStore();
+          await routerStore.SetAsyncRouter();
+          const asyncRouters = routerStore.asyncRouters;
+          asyncRouters.forEach((asyncRouter) => {
+            router.addRoute(asyncRouter);
+          });
+          router.push({ name: this.userInfo.authority.defaultRouter });
+          return true;
         }
-    } catch (e) {
+      } catch (e) {
         loadingInstance.close();
+      }
+      loadingInstance.close();
     }
-    loadingInstance.close();
   }
-})
+}, () => originalUserStore)
