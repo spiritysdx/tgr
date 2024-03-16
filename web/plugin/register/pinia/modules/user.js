d6fdfc0eb0e4111944897a4e8e745404b57ee93c
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-03-16 17:26:33 +0800

    Update user.js

--- web/plugin/register/pinia/modules/user.js
+++ web/plugin/register/pinia/modules/user.js
@@ -1,42 +1,38 @@
 import { useUserStore } from '@/pinia/modules/user'
-import { ElLoading } from 'element-plus'
+import { ElLoading, ElMessage } from 'element-plus'
 import { userRegister } from '@/plugin/register/api/api'
 import { defineStore } from 'pinia'
 
-// 获取原始的 useUserStore 模块
-const originalUserStore = useUserStore()
-
-// 扩展原始的 useUserStore 模块
 export const extendedUseUserStore = defineStore({
-  id: 'user',
+  id: 'extendedUser',
   state: () => ({
-    userInfo: null,
-    token: null
+    originalUserStore: useUserStore(),
+    loadingInstance: null
   }),
   actions: {
     async Register(loginInfo) {
-      const loadingInstance = ElLoading.service({
+      this.loadingInstance = ElLoading.service({
         fullscreen: true,
         text: "注册中，请稍候...",
       });
       try {
         const res = await userRegister(loginInfo);
         if (res.code === 0) {
-          this.userInfo = res.data.user;
-          this.token = res.data.token;
-          const routerStore = this.useRouterStore();
+          this.originalUserStore.userInfo = res.data.user;
+          this.originalUserStore.token = res.data.token;
+          const routerStore = this.originalUserStore.useRouterStore();
           await routerStore.SetAsyncRouter();
           const asyncRouters = routerStore.asyncRouters;
           asyncRouters.forEach((asyncRouter) => {
             router.addRoute(asyncRouter);
           });
-          router.push({ name: this.userInfo.authority.defaultRouter });
+          router.push({ name: this.originalUserStore.userInfo.authority.defaultRouter });
           return true;
         }
       } catch (e) {
-        loadingInstance.close();
+        this.loadingInstance.close();
       }
-      loadingInstance.close();
+      this.loadingInstance.close();
     }
   }
-}, () => originalUserStore)
+})
